5d1a34091986592b66759d1c022c95430a8ce341
LucidJoy <jyotirmoykarmakar02@gmail.com>
2022-06-14 12:19:03 +0530

    MainMint component design & minor fixes for prod

---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+export { default as FacebookIcon } from "./social-media-icons/facebook_32x32.png";
+export { default as TwitterIcon } from "./social-media-icons/twitter_32x32.png";
+export { default as EmailIcon } from "./social-media-icons/email_32x32.png";
+export { default as LinkedIn } from "./social-media-icons/linkedin_32x32.png";
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Box, Button, Flex, Image, Link, Spacer } from "@chakra-ui/react";
+
+import { FacebookIcon, TwitterIcon, EmailIcon, LinkedIn } from "../assets";
 
 const NavBar = ({ accounts, setAccounts }) => {
   const isConnected = Boolean(accounts[0]);
@@ -13,21 +16,92 @@ const NavBar = ({ accounts, setAccounts }) => {
   };
 
   return (
-    <div>
-      <div>Facebook</div>
-      <div>Twitter</div>
-      <div>Email</div>
+    <Flex justify='space-between' align='center' padding='15px 30px'>
+      <Flex justify='space-around' width='30%' padding='0 25px'>
+        <Link href='https://www.facebook.com'>
+          <Image
+            src={FacebookIcon}
+            boxSize='42px'
+            margin='0 10px'
+            _hover={{ transform: "scale(1.05)" }}
+          />
+        </Link>
+        <Link href='https://twitter.com/LucidJoyy'>
+          <Image
+            src={TwitterIcon}
+            boxSize='42px'
+            margin='0 10px'
+            _hover={{ transform: "scale(1.05)" }}
+          />
+        </Link>
+        <Link href='https://github.com/LucidJoy'>
+          <Image
+            src={EmailIcon}
+            boxSize='42px'
+            margin='0 10px'
+            _hover={{ transform: "scale(1.05)" }}
+          />
+        </Link>
+      </Flex>
 
-      <div>About</div>
-      <div>Mint</div>
-      <div>Team</div>
+      <Flex
+        justify='space-around'
+        align='center'
+        width='40%'
+        padding='30px 30px 30px 30px'
+      >
+        <Box
+          margin='0 15px'
+          _hover={{ transform: "scale(1.05)", cursor: "pointer" }}
+        >
+          About
+        </Box>
+        <Spacer />
+        <Box
+          margin='0 15px'
+          _hover={{ transform: "scale(1.05)", cursor: "pointer" }}
+        >
+          Mint
+        </Box>
+        <Spacer />
+        <Box
+          margin='0 15px'
+          _hover={{ transform: "scale(1.05)", cursor: "pointer" }}
+        >
+          Team
+        </Box>
+        <Spacer />
 
-      {isConnected ? (
-        <p>Connected</p>
-      ) : (
-        <button onClick={connectAccount}>Connect</button>
-      )}
-    </div>
+        {isConnected ? (
+          <Box
+            margin='0 15px'
+            inset='0'
+            border='2px solid #D6517D'
+            borderRadius='5px'
+            padding='6px 12px'
+            userSelect='none'
+          >
+            Connected
+          </Box>
+        ) : (
+          <Button
+            backgroundColor='#D6517D'
+            borderRadius='5px'
+            boxShadow='0px 2px 2px 1px #0F0F0F'
+            color='white'
+            cursor='pointer'
+            fontFamily='inherit'
+            Family='inherit'
+            padding='15px'
+            margin='0 15px'
+            onClick={connectAccount}
+            _hover={{ backgroundColor: "#c14971" }}
+          >
+            Connect
+          </Button>
+        )}
+      </Flex>
+    </Flex>
   );
 };
 
